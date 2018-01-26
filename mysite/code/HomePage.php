736720f0ad4400c2153e40f1172a5100615864b9
pbortnick <pbortnick@gmail.com>
2018-01-25 21:36:12 -0500

    added userforms module

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -1,5 +1,7 @@
 <?php
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig;
 
 class HomePage extends Page
 {
@@ -9,9 +11,11 @@ class HomePage extends Page
 
   public function getCMSFields() {
     $f = parent::getCMSFields();
-    $config = GridFieldConfig_RelationEditor::create();
-    $grid = GridField::create('ComparisonTopLevelImages', 'Images', $this->ComparisonTopLevelImages(), $config);
-    $f->addFieldToTab('Root.Main', $grid);
+    $f->addFieldToTab('Root.Pages',
+    new GridField('Pages', 'All pages', SiteTree::get()));
+    $config = GridFieldConfig_RecordEditor::create();
+    // $config = GridFieldConfig_RecordEditor::create();
+    // $grid = GridField::create('ComparisonTopLevelImages', 'Images', $this->ComparisonTopLevelImages(), $config);
     return $f;
   }
 }
